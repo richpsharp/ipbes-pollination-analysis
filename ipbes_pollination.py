0a06dbc92e3e4b0a5ca7e5b19490c3756407a532
Rich Sharp <richpsharp@gmail.com>
2018-07-28 19:21:29 -0500

    increasing cpu count for analysis

--- ipbes_pollination.py
+++ ipbes_pollination.py
@@ -56,7 +56,7 @@ CHURN_DIR = os.path.join(WORKING_DIR, 'churn')
 GOOGLE_BUCKET_KEY_PATH = "ecoshard-202992-key.json"
 NODATA = -9999
 # I've found the taskgraph at most gets about 32 tasks wide
-N_WORKERS = min(32, multiprocessing.cpu_count())
+N_WORKERS = max(1, multiprocessing.cpu_count())
 DELAYED_START = N_WORKERS >= 0
 
 GOOGLE_BUCKET_ID = 'ipbes-pollination-result'

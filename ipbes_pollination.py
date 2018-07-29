00047bfdd9c0bc44e898b792e96ceb6cd316ce90
Rich Sharp <richpsharp@gmail.com>
2018-07-28 19:43:53 -0500

    removing old comment

--- ipbes_pollination.py
+++ ipbes_pollination.py
@@ -55,7 +55,6 @@ ECOSHARD_DIR = os.path.join(WORKING_DIR, 'ecoshard_dir')
 CHURN_DIR = os.path.join(WORKING_DIR, 'churn')
 GOOGLE_BUCKET_KEY_PATH = "ecoshard-202992-key.json"
 NODATA = -9999
-# I've found the taskgraph at most gets about 32 tasks wide
 N_WORKERS = max(1, multiprocessing.cpu_count())
 DELAYED_START = N_WORKERS >= 0
 
